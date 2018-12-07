d7cd05c04d9a37cba58894d7fa666700345d2cf4
CKaii <44842433+CKaii@users.noreply.github.com>
2018-12-07 15:56:59 -0500

    minor-issue-fix
    added a function to calculate and made a while loop to make sure only operators would pass

--- calculator.py
+++ calculator.py
@@ -1,10 +1,12 @@
-operations = ["add","subtract","multiply","divide","power", "+", "-", "*", "/", "**"]
-value1 = input("Please enter your first number: ")
-value2 = input("Please enter your second number: ")
-question = input("What would you like to do? ")
+operators = ["add", "subtract", "multiply", "divide", "power", "+", "-", "*", "/", "**"]
 
-value1_float = float(value1)
-value2_float= float(value2)
+float_value_1 = float(input("Please enter your first number: "))
+float_value_2 = float(input("Please enter your second number: "))
+user_operation = input("What would you like to do? ")
+
+while user_operation not in operators:
+    print("This operation is not yet available, please choose between addition, multiplaction, subtraction, division, or exponentiation")
+    user_operation = input("What would you like to do? ")
 
 def add(num1=0, num2=0):
     return num1+num2
@@ -21,16 +23,18 @@ def divide(num1=0, num2=0):
 def power(num1=1, num2=0):
     return num1**num2
 
-while question not in operations:
-    print("Please input a math operation")
-    question = input("What would you like to do? ")
-if question in operations == "add" or operations == "+":
-    print(add(value1_float, value2_float))
-elif question in operations =="subtract" or operations =="-":
-    subtract(value1_float, value2_float)
-elif question in operations =="divide" or operations =="/":
-    divide(value1_float, value2_float)
-elif question in operations =="multiply" or operations=="*":
-    multiply(value1_float, value2_float)
-else:
-    power(value1_float, value2_float)
+def calculate():
+    if user_operation == "add" in operators or user_operation == "+":
+        print(add(float_value_1, float_value_2))
+    elif user_operation == "subtract" in operators or user_operation == "-":
+        print(subtract(float_value_1, float_value_2))
+    elif user_operation == "divide" in operators or user_operation == "/":
+        print(divide(float_value_1, float_value_2))
+    elif user_operation == "multiply" in operators or user_operation == "*":
+        print(multiply(float_value_1, float_value_2))
+    elif user_operation == "power" in operators or user_operation == "**":
+        print(power(float_value_1, float_value_2))
+    else:
+        print("This calculator does not support this function")
+
+calculate()
